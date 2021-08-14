6f51b8d36fca9534de790cf87d8fe46b85961513
Dilan Silva <sdilansilva@gmail.com>
2021-08-14 13:28:26 +0530

    set text color for error message

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -19,14 +19,17 @@ const Login = () => {
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-
+            <Form.Text className="red-color-text">
+              {error.email}
             </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
+            <Form.Text className="red-color-text">
+              {error.password}
+            </Form.Text>
           </Form.Group>
 
           <Button variant="primary" type="submit">
