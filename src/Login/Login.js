9a1955cd658380e635a3bc5efa58c32d4b4c7c9a
Dilan Silva <sdilansilva@gmail.com>
2021-08-14 13:16:01 +0530

    code changes

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,38 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
 
-
-import {Form,Button} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
-    return (
-        <div className="main-header">
-            <div className="inner-header">
-            <h1>Login</h1>
-            <br/>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+  return (
+    <div className="main-header">
+      <div className="inner-header">
+        <h1>Login</h1>
+        <br />
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
-        
+
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-        
+
           <Button variant="primary" type="submit">
             Login
           </Button>
-            </Form>
-            </div>
-        </div>
-    )
-}
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
