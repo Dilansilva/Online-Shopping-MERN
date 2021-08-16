ea72e7c16efd87e4b6920af093118216c450167e
Dilan Silva <sdilansilva@gmail.com>
2021-08-16 17:31:13 +0530

    main input component is created

--- src/Login/InputComponent.js
+++ src/Login/InputComponent.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Form } from "react-bootstrap";
 
-const InputComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const InputComponent = (props) => {
+  return (
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>{props.name}</Form.Label>
+      <Form.Control type={props.type} placeholder={props.placeholder} />
+      <Form.Text className="red-color-text">{props.error}</Form.Text>
+    </Form.Group>
+  );
+};
 
-export default InputComponent
+export default InputComponent;
